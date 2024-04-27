024150709d0b1f6aa62cf6587708e74cb8e0fbbf
MD. AL-AMIN <amin73288@gmail.com>
2024-04-28 01:13:16 +0600

    Admin registered- Medicines, CartItem, Bill, BillItem

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-
+admin.site.register(CartItem)
+admin.site.register(Medicines)
+admin.site.register(Bill)
+admin.site.register(BillItem)
 admin.site.register(Doctor)
 admin.site.register(DoctorTimeSlot)
 admin.site.register(Appointment)
