a5e68ffac7dbd49452a38e8e21925788d3f6a3f9
MD. AL-AMIN <amin73288@gmail.com>
2024-04-28 00:03:32 +0600

    Medicine Class Updated

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete
+from django.utils import timezone
 
 # Create your models here.
+class Medicines(models.Model):
+    def __str__(self):
+        return self.p_name
+
+    p_image = models.ImageField()
+    p_name = models.CharField(max_length=100)
+    p_description = models.CharField(max_length=1000)
+    p_cost = models.IntegerField()
+    p_count = models.IntegerField()
+    v_name = models.CharField(max_length=100)
+    v_description = models.CharField(max_length=100)
