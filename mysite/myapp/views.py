a1395c157ac23434896313890349687223d28d7a
Saifcodex <saifahmed3693@gmail.com>
2024-04-28 00:17:09 +0600

    Views Initial import files added

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.urls import reverse
+from django.db.models import Prefetch
+from .models import *
+from django.shortcuts import (
+    get_object_or_404,
+    redirect,
+    render
+)
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import (
+    authenticate,
+    login as auth_login,
+    logout as auth_logout
+)
+from .forms import Captcha, UserProfileForm, UserForm
+from django.db.models import Q
 
-# Create your views here.
+
+# Shift kora function
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def FAQ(request):
+    return render(request, 'FAQ.html')
