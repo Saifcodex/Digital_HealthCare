17c6c0ebbf6247ffa3dbf3237400b2d8fba12cd2
MD. AL-AMIN <amin73288@gmail.com>
2024-04-28 02:59:31 +0600

    register function added

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -62,6 +62,43 @@ def login(request):
     return render(request, "login.html", {"form": form})
 
 
+def register(request):
+    if request.method == "POST":
+        form = Captcha(request.POST)
+        if form.is_valid():
+            u_name = request.POST.get("u_name")
+            u_fname = request.POST.get("u_fname")
+            u_lname = request.POST.get("u_lname")
+            u_email = request.POST.get("u_email")
+            u_password = request.POST.get("u_password")
+            u_age = request.POST.get("u_age")
+            u_address = request.POST.get("u_address")
+            u_mobile = request.POST.get("u_mobile")
+            u_gender = request.POST.get("u_gender")
+
+            user = User.objects.create_user(username=u_name, first_name=u_fname, last_name=u_lname, email=u_email,
+                                            password=u_password)
+            user.save()
+            user_profile = UserProfile(user=user, age=u_age, address=u_address, mobile=u_mobile, gender=u_gender)
+            user_profile.save()
+
+            authenticated_user = authenticate(request, username=u_name, password=u_password)
+
+            if authenticated_user is not None:
+                auth_login(request, authenticated_user)
+                messages.success(request, "Your account has been successfully created.")
+                return redirect("home")
+            else:
+                messages.error(request, "User registration failed. Please try again.")
+        else:
+            messages.error(request, "reCAPTCHA verification failed. Please try again.")
+
+    else:
+        form = Captcha()
+
+    return render(request, "register.html", {"form": form})
+
+
 
 
 
