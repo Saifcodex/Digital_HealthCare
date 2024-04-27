65c7fcc5ef1757e1ae69ff1232cfb5481ca77e0a
Your Name <you@example.com>
2024-04-28 00:22:37 +0600

    doctor search function added

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -44,3 +44,40 @@ def doctors(request):
         'doctors': doctors
     }
     return render(request, "doctors.html", context)
+
+
+def doctor_search(request):
+    query_d = request.GET.get('q')
+
+    if query_d:
+        words = query_d.split()
+        name_query = Q()
+        specialty_query = Q()
+        status_query = Q()
+
+        for word in words:
+            if word.lower() == "available":
+                status_query = Q(status=True)
+
+            elif word.lower() == "unavailable":
+                status_query = Q(status=False)
+
+            else:
+                name_query |= Q(name__icontains=word)
+                specialty_query |= Q(specialty__icontains=word)
+
+        doctors = Doctor.objects.filter(name_query | specialty_query, status_query)
+    else:
+        messages.error(request, "Search bar was empty")
+        return redirect('doctors')
+
+    if not doctors:
+        messages.error(request, "No doctors found.")
+        return redirect('doctors')
+
+    context = {
+        'doctors': doctors,
+    }
+
+    return render(request, 'doctors.html', context)
+
