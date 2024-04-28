4f9d1749ce741cfc185c9434cea7ef91cfaa6e53
Saifcodex <saifahmed3693@gmail.com>
2024-04-28 16:42:20 +0600

    Check the forms and make some reformating

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -3,15 +3,18 @@ from .models import UserProfile
 from django.contrib.auth.models import User
 from django_recaptcha.fields import ReCaptchaField
 
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['age', 'address', 'mobile', 'gender']
 
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
+
 
 class Captcha(forms.Form):
     captcha = ReCaptchaField()
